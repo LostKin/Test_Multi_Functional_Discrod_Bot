c82f3b3d11aceb979414785f3227a63aa1f15fe9
Sasha_Drozdov <drsasha7@mail.ru>
2019-07-30 17:35:33 +0300

    почти рабочая аудио версия

--- test.py
+++ test.py
@@ -1,15 +1,34 @@
 import dbl
 import discord
+import youtube_dl
 from discord.ext import commands
 
 import asyncio
 import requests
 import logging
 
-DISCORD_BOT_TOKEN = 'NjA1NDgxOTQ5Nzc5NDYwMDk4.XT9S9g.eFuf9dlQgYqRY-yzeA1KTFUDZbw'
-BOT_NAME = 'TestBot#9545'
+import multiprocessing
+import time
 
-client = discord.Client()
+#discord.Message().
+
+players = {}
+
+DISCORD_BOT_TOKEN = 'NjA1NDgxOTQ5Nzc5NDYwMDk4.XT9Vzg.JZgvKNjxF9UcZziEih4_2aO3iDo'
+BOT_NAME = 'TestBot#9545'
+CREATOR_NAME = 'Sasha Drozdov#8680'
+
+#client = discord.Client()
+client = commands.Bot(command_prefix='?')
+vclient = 0
+source = 0
+
+def play_music():
+    global vclient, source
+    while (True):
+        vclient.play(source)
+        source.read()
+    #voice_client.play(source)
 
 @client.event
 
@@ -20,10 +39,35 @@ async def on_ready():
     print('------')
     
 @client.event
-async def on_message(message):
-    if message.author != BOT_NAME and message.content.startswith('Привет, TestBot'):
-        print('[command]: test ')
-        await message.channel.send('И тебе привет, %s.' % message.author)
-        
+async def on_message(message:discord.Message):
+    global vclient
+    if vclient.is_playing:
+        vclient.stop()
+    return  
 
+@client.event
+async def on_voice_state_update(member, before:discord.VoiceState, after:discord.VoiceState):
+    global vclient, source
+    if (str(member) == BOT_NAME):
+        return
+    if (after.channel == None):
+        if (vclient.is_connected):
+            await vclient.disconnect()            
+    if (after.channel != None):
+        if (vclient == 0):
+            vclient =  await after.channel.connect()
+        else:
+            if vclient.is_connected:
+                await vclient.disconnect()
+            vclient =  await after.channel.connect()
+        #source = discord.FFmpegPCMAudio("https://www.youtube.com/watch?v=hjGZLnja1o8")
+        source = discord.FFmpegPCMAudio("/home/sasha/TestBot/music/output.avi")
+        vclient.play(source)
+        await asyncio.sleep(5)
+        vclient.stop()
+        #await vclient.disconnect()
+    return
+
+if discord.opus.is_loaded():
+    print("OK")
 client.run(DISCORD_BOT_TOKEN)
